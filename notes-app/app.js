3dd90d0a0722eac013a0b6c2b3823f7326ec3472
amit9031jsr <amit9031jsr@gmail.com>
2019-10-05 07:54:27 +0530

    Importing npm modules and printing in color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,18 @@
+// const validator = require('validator')
+
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 
 console.log(msg)
+
+const greenMsg = chalk.green.inverse.bold('Sucess!')
+console.log(greenMsg)
+
+// console.log(chalk.red('Danger'))
+// console.log(chalk.yellow('Warning'))
+// console.log(validator.isEmail('amit@gmail.com'))
 
 // const firstName = require('./utils.js')
 // const add = require('./utils.js')
