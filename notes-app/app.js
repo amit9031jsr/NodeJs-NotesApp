e2fa95703b2058249a95d8d1dd7560e8efeeede4
amit9031jsr <amit9031jsr@gmail.com>
2019-10-06 02:17:29 +0530

    Argument parsing with yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,29 +1,59 @@
-// const validator = require('validator')
-
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
+// Customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding notes!')
-}else if (command === 'remove') {
-    console.log('Removing notes!')
-}
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
 
-// const msg = getNotes()
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
 
-// console.log(msg)
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all note')
+    }
+})
 
-// const greenMsg = chalk.green.inverse.bold('Sucess!')
-// console.log(greenMsg)
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
 
-// console.log(chalk.red('Danger'))
-// console.log(chalk.yellow('Warning'))
-// console.log(validator.isEmail('amit@gmail.com'))
+// console.log(yargs.argv)
 
-// const firstName = require('./utils.js')
-// const add = require('./utils.js')
-// const sum = add(2, 4)
-// console.log(firstName)
-// console.log(sum)
+yargs.parse()
